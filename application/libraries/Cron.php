c370cebce5cafd247833268b4cb3892dbfa577e9
Yordano <yordanoweb@yahoo.es>
2018-10-08 15:28:04 -0400

    indent a cuatro espacios. los editores ahora quieren cuatro espacios.

--- application/libraries/Cron.php
+++ application/libraries/Cron.php
@@ -2,22 +2,22 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cron {
-
-  public function is_time($interval) {
-    $hour = (int) date('H');
-    $mins = (int) date('i');
-    if ($mins !== 0) { return false; }
-    return $hour % (int) $interval === 0;
-  }
-
-  /**
-   * Returns date and time in log format: MMM DD HH:mm:ss
-   */
-  public function time_str() {
-    $d = date('j');
-    return sprintf("%s %s %s", date('M'),
-      strlen($d) === 2 ? $d : ' ' . $d,
-      date('G:i:s'));
-  }
-  
+    
+    public function is_time($interval) {
+        $hour = (int) date('H');
+        $mins = (int) date('i');
+        if ($mins !== 0) { return false; }
+        return $hour % (int) $interval === 0;
+    }
+    
+    /**
+    * Returns date and time in log format: MMM DD HH:mm:ss
+    */
+    public function time_str() {
+        $d = date('j');
+        return sprintf("%s %s %s", date('M'),
+        strlen($d) === 2 ? $d : ' ' . $d,
+        date('G:i:s'));
+    }
+    
 }
